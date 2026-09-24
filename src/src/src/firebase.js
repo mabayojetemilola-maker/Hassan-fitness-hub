746cb5ecb06ea832d5e0e3f2a03c4a3cbb64b586
mabayojetemilola-maker <mabayojetemilola@gmail.com>
2026-09-24 07:36:10 +0100

    Update firebase.js

--- src/src/src/firebase.js
+++ src/src/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC7KGgRRPwDUDJuC7m3vsLBdQu0P_jckMk",
+  authDomain: "hassan-fitness-hub.firebaseapp.com",
+  projectId: "hassan-fitness-hub",
+  storageBucket: "hassan-fitness-hub.firebasestorage.app",
+  messagingSenderId: "1053109150100",
+  appId: "1:1053109150100:web:74db778a9bd2299acb2f81"
 };
 
 const app = initializeApp(firebaseConfig);
